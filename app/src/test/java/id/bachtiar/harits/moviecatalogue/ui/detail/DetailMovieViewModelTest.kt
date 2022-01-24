bbb25ac7be99b7ba76e586a6456bfa9038cd7e04
Harits <harits@sekolahmu.co.id>
2022-01-24 17:15:01 +0700

    Update view model test

--- app/src/test/java/id/bachtiar/harits/moviecatalogue/ui/detail/DetailMovieViewModelTest.kt
+++ app/src/test/java/id/bachtiar/harits/moviecatalogue/ui/detail/DetailMovieViewModelTest.kt
@@ -1,6 +1,8 @@
 package id.bachtiar.harits.moviecatalogue.ui.detail
 
+import id.bachtiar.harits.moviecatalogue.model.Cast
 import id.bachtiar.harits.moviecatalogue.model.Movie
+import id.bachtiar.harits.moviecatalogue.model.SubDesc
 import org.junit.Assert.*
 import org.junit.Before
 
@@ -20,13 +22,26 @@ class DetailMovieViewModelTest {
             description = "description spiderman",
             releaseDate = "release_date",
             rating = 90,
-            totalUserRating = 100
+            totalUserRating = 100,
+            category = listOf("Fantasy", "Action"),
+            subDesc = listOf(SubDesc(title = "Director", description = "Sam Raimi")),
+            cast = listOf(Cast(name = "Tobey Maguire", roleName = "Peter Parker", avatar = "url_avatar"))
         )
     }
 
     @Test
     fun getMovie() {
+        detailMovieViewModel.movie = Movie()
         assertNotNull(detailMovieViewModel.movie)
+        assertEquals(detailMovieViewModel.movie.title, movie.title)
+        assertEquals(detailMovieViewModel.movie.cover, movie.cover)
+        assertEquals(detailMovieViewModel.movie.description, movie.description)
+        assertEquals(detailMovieViewModel.movie.releaseDate, movie.releaseDate)
+        assertEquals(detailMovieViewModel.movie.rating, movie.rating)
+        assertEquals(detailMovieViewModel.movie.totalUserRating, movie.totalUserRating)
+        assertEquals(detailMovieViewModel.movie.category, movie.category)
+        assertEquals(detailMovieViewModel.movie.subDesc, movie.subDesc)
+        assertEquals(detailMovieViewModel.movie.cast, movie.cast)
     }
 
     @Test
--- app/src/test/java/id/bachtiar/harits/moviecatalogue/ui/detail/DetailTvShowViewModelTest.kt
+++ app/src/test/java/id/bachtiar/harits/moviecatalogue/ui/detail/DetailTvShowViewModelTest.kt
@@ -1,5 +1,7 @@
 package id.bachtiar.harits.moviecatalogue.ui.detail
 
+import id.bachtiar.harits.moviecatalogue.model.Cast
+import id.bachtiar.harits.moviecatalogue.model.SubDesc
 import id.bachtiar.harits.moviecatalogue.model.TvShow
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
@@ -20,13 +22,27 @@ class DetailTvShowViewModelTest {
             description = "description Arrow",
             releaseDate = "release_date",
             rating = 90,
-            totalUserRating = 100
+            totalUserRating = 100,
+            category = listOf("Fantasy", "Adventure"),
+            subDesc = listOf(SubDesc(title = "Creator", description = "Rowell")),
+            cast = listOf(Cast(name = "Stephen Amell", roleName = "Oliver Queen", avatar = "url_avatar"))
         )
     }
 
     @Test
     fun getTvShow() {
+        detailTvShowViewModel.tvShow = tvShow
         assertNotNull(detailTvShowViewModel.tvShow)
+        assertNotNull(detailTvShowViewModel.tvShow)
+        assertEquals(detailTvShowViewModel.tvShow.title, tvShow.title)
+        assertEquals(detailTvShowViewModel.tvShow.cover, tvShow.cover)
+        assertEquals(detailTvShowViewModel.tvShow.description, tvShow.description)
+        assertEquals(detailTvShowViewModel.tvShow.releaseDate, tvShow.releaseDate)
+        assertEquals(detailTvShowViewModel.tvShow.rating, tvShow.rating)
+        assertEquals(detailTvShowViewModel.tvShow.totalUserRating, tvShow.totalUserRating)
+        assertEquals(detailTvShowViewModel.tvShow.category, tvShow.category)
+        assertEquals(detailTvShowViewModel.tvShow.subDesc, tvShow.subDesc)
+        assertEquals(detailTvShowViewModel.tvShow.cast, tvShow.cast)
     }
 
     @Test
